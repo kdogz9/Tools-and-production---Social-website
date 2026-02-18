7be5b15c550dd1c5067dcb70096ca6d6fd429e90
kdogz9 <2401317@students.ucreative.ac.uk>
2026-02-18 14:34:41 +0000

    added more pages

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-// Set the date we're counting down to
-const launchDate = new Date("March 1, 2026 00:00:00").getTime();
+// Set your release date here (Year, Month - 1, Day, Hour, Minute)
+const launchDate = new Date("April 24, 2026 12:00:00").getTime();
 
 const timer = setInterval(function() {
     const now = new Date().getTime();
     const distance = launchDate - now;
 
+    // Time calculations
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
+    // Output the results
     document.getElementById("days").innerText = days;
     document.getElementById("hours").innerText = hours;
     document.getElementById("mins").innerText = minutes;
 
+    // If countdown is finished
     if (distance < 0) {
         clearInterval(timer);
-        document.getElementById("countdown").innerHTML = "OUT NOW!";
+        document.getElementById("countdown").innerHTML = "<h2 style='color:var(--gold)'>RELEASED!</h2>";
     }
 }, 1000);
